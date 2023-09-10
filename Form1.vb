d7ec9510b53fddc9a9f6132fec4f19d544ccd787
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-10 14:10:30 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnResults_Click(sender As Object, e As EventArgs) Handles btnResults.Click
+        'GO TO ERROR
+        Dim isNumber As String
+        isNumber = txtCharecters.Text
+
+
+        On Error GoTo ErrorHandler
+
+        'If entered a numeric number
+        Dim numericValue As Double
+        numericValue = CDbl(isNumber)
+
+        'If entered a numeric number display the results
+        MsgBox("Valid: " & numericValue)
+        Exit Sub
+ErrorHandler:
+        'If entered a non-numeric number display results
+        MsgBox("Error: " & isNumber & " Is Not a number")
+    End Sub
+End Class
